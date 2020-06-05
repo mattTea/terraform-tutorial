a9e0edc2b0a13be7174c788a6820019c19df9d30
Darren Oakley <darren.oakley@johnlewis.co.uk>
2020-06-05 14:38:29 +0100

    Fixed a bunch of syntax errors

--- step-by-step/step-3-local-variables-and-count/storage.tf
+++ step-by-step/step-3-local-variables-and-count/storage.tf
@@ -1,13 +1,13 @@
 # Now we have two buckets
 
 resource "google_storage_bucket" "bucket-for-project-config" {
-  name               = "configuration-files-for-project"
-  location           = "EU"
+  name     = "configuration-files-for-project"
+  location = "EU"
 }
 
 resource "google_storage_bucket" "bucket-for-project-data" {
-  name               = "data-files-for-project"
-  location           = "EU"
+  name     = "data-files-for-project"
+  location = "EU"
 }
 
 # Service account as before
@@ -21,14 +21,17 @@ resource "google_service_account" "account-that-will-read-bucket" {
 
 # a local variable with a list of the buckets in it
 locals {
-  all_buckets = [google_storage_bucket.bucket-for-project-config, google_storage_bucket.bucket-for-project-data]
+  all_buckets = [
+    "${google_storage_bucket.bucket-for-project-config}",
+    "${google_storage_bucket.bucket-for-project-data}",
+  ]
 }
 
 # Use "count" to apply this to each of the buckets in the list
 # This will result in two resources being created, called "account-bucket-access[0]" and "account-bucket-access[1]"
 resource "google_storage_bucket_iam_member" "account-bucket-access" {
-  count = local.all_buckets.count
-  bucket = local.all_buckets[count.index]
+  count  = "${local.all_buckets.count}"
+  bucket = "${local.all_buckets[count.index]}"
   role   = "roles/storage.objectViewer"
   member = "serviceAccount:${google_service_account.account-that-will-read-bucket.email}"
 }
