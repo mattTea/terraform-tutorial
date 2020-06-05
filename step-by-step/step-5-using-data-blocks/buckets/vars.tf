2f16c5214a0b45c106fc859d8719dba82a3fab49
Stephen Tjasink <stephen.tjasink@johnlewis.co.uk>
2020-06-05 15:09:48 +0100

    Adding step 5 for data blocks

---
+++ step-by-step/step-5-using-data-blocks/buckets/vars.tf
@@ -0,0 +1,7 @@
+variable "env_name" {
+  description = "The environment we are deploying to"
+}
+
+variable "service_account_name" {
+  description = "The name of the service account to grant access to"
+}
---
+++ step-by-step/step-5-using-data-blocks/environment.tf
@@ -0,0 +1,13 @@
+locals {
+  env_name = "prod"
+}
+
+data "google_service_account" "existing-service-account" {
+  account_id = "${local.env_name}-microservice-name"
+}
+
+module "buckets-and-account" {
+  source = "buckets"
+  env_name = local.env_name
+  service_account_name = data.google_service_account.existing-service-account
+}
