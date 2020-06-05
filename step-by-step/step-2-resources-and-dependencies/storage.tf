b6bd588ba95cad7369dfef6823c397bfeea68edd
Darren Oakley <darren.oakley@johnlewis.co.uk>
2020-06-05 14:41:41 +0100

    Revert "Fixed a bunch of syntax errors"
    This reverts commit a9e0edc2b0a13be7174c788a6820019c19df9d30.

--- step-by-step/step-2-resources-and-dependencies/storage.tf
+++ step-by-step/step-2-resources-and-dependencies/storage.tf
@@ -1,8 +1,8 @@
 # Bucket as in step 1
 
 resource "google_storage_bucket" "bucket-for-project-config" {
-  name     = "configuration-files-for-project"
-  location = "EU"
+  name               = "configuration-files-for-project"
+  location           = "EU"
 }
 
 # Also create a service account
@@ -16,7 +16,7 @@ resource "google_service_account" "account-that-will-read-bucket" {
 # resources, as this ensures that the bucket and account are created before the permission is applied
 
 resource "google_storage_bucket_iam_member" "account-bucket-access" {
-  bucket = "${google_storage_bucket.bucket-for-project-config.name}"
+  bucket = google_storage_bucket.bucket-for-project-config.name
   role   = "roles/storage.objectViewer"
   member = "serviceAccount:${google_service_account.account-that-will-read-bucket.email}"
 }
--- step-by-step/step-4-variables-and-modules/buckets/storage.tf
+++ step-by-step/step-4-variables-and-modules/buckets/storage.tf
@@ -1,13 +1,13 @@
 # The bucket and service account names are now prefixed with environment name
 
 resource "google_storage_bucket" "bucket-for-project-config" {
-  name     = "${var.env_name}-configuration-files-for-project"
-  location = "EU"
+  name               = "${var.env_name}-configuration-files-for-project"
+  location           = "EU"
 }
 
 resource "google_storage_bucket" "bucket-for-project-data" {
-  name     = "${var.env_name}-data-files-for-project"
-  location = "EU"
+  name               = "${var.env_name}-data-files-for-project"
+  location           = "EU"
 }
 
 resource "google_service_account" "account-that-will-read-bucket" {
@@ -18,15 +18,12 @@ resource "google_service_account" "account-that-will-read-bucket" {
 # it is reading the names from the resources above and will get the correct names from them.
 
 locals {
-  all_buckets = [
-    "${google_storage_bucket.bucket-for-project-config}",
-    "${google_storage_bucket.bucket-for-project-data}",
-  ]
+  all_buckets = [google_storage_bucket.bucket-for-project-config, google_storage_bucket.bucket-for-project-data]
 }
 
 resource "google_storage_bucket_iam_member" "account-bucket-access" {
-  count  = "${local.all_buckets.count}"
-  bucket = "${local.all_buckets[count.index]}"
+  count = local.all_buckets.count
+  bucket = local.all_buckets[count.index]
   role   = "roles/storage.objectViewer"
   member = "serviceAccount:${google_service_account.account-that-will-read-bucket.email}"
 }
